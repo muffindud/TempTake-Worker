605fef9072a0975ef55edf9275557ed986edfed9
muffindud <ccatlabuga2002@gmail.com>
2024-12-01 17:25:10 +0200

    update mcu

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,25 @@
 #include <Adafruit_BMP085.h>
 #include <Wire.h>
 #include <DHT.h>
-#include <SoftwareSerial.h>
 
 Adafruit_BMP085 bmp;
 DHT dht;
-SoftwareSerial HC12(HC_12_RX_PIN, HC_12_TX_PIN);
 
-int delaySeconds = 1;
+int delaySeconds = 2;
 
 bool dht_active = false;
 bool bmp_active = false;
-bool rain_active = false;
+bool mq_active = false;
 
 String data = "";
 int p = 0;
 
 void setup(){
-    Serial.begin(9600);
-    HC12.begin(HC_12_BAUD_RATE);
+    Serial.begin(HC_12_BAUD_RATE);
 
     bmp_active = bmp.begin();
 
     delay(500);
-
-    pinMode(RAIN_SENSOR_PIN, INPUT);
-    rain_active = true;
 
     dht.setup(DHT_22_PIN);
     dht_active = dht.getModel() != DHT::AUTO_DETECT;
@@ -40,12 +34,17 @@ void loop(){
     // if(HC12.available()){
     //     Serial.write(HC12.read());
     // }
+
     data = "";
 
     if(dht_active){
         data += "T: " + String(dht.getTemperature()) + "C";
         data += "\n\r";
         data += "H: " + String(dht.getHumidity()) + "%";
+        data += "\n\r";
+        data += "\n\r";
+    }else{
+        data += "DHT22 NO";
         data += "\n\r";
         data += "\n\r";
     }
@@ -56,17 +55,16 @@ void loop(){
         data += "T: " + String(bmp.readTemperature()) + "C";
         data += "\n\r";
         data += "\n\r";
-    }
-
-    if(rain_active){
-        data += "R: " + String(analogRead(RAIN_SENSOR_PIN));
+    }else{
+        data += "BMP180 NO";
+        data += "\n\r";
         data += "\n\r";
     }
 
     data += String(p);
     p = (p + 1) % 10000;
 
-    HC12.println(data);
+    Serial.println(data);
 
     delay(delaySeconds * 1000);
 }
