94c9820062971c8692a2f4e9a4cc2b6754e0c6be
muffindud <ccatlabuga2002@gmail.com>
2025-01-21 21:36:18 +0200

    add basic data packing functionality

--- lib/DataPacker/DataPacker.cpp
+++ lib/DataPacker/DataPacker.cpp
@@ -1,8 +1,50 @@
 #include "DataPacker.h"
 
 
-#define START_BYTE 0x7E
-#define END_BYTE 0x7F
+#define DAT_MODE 0x0D
+#define ACK_MODE 0x0A
 
-#define SYN_MODE 0x00
-#define ACK_MODE 0x01
+u8_t* dataToStream(DAT_T data){
+    u8_t* stream = (u8_t*)malloc(DAT_SIZE + data.length);
+    stream[0] = data.type;
+    stream[1] = (data.meta.crc16 >> 8) & 0xFF;
+    stream[2] = data.meta.crc16 & 0xFF;
+    for (int i = 0; i < 6; i++){
+        stream[3 + i] = data.meta.manager_mac[i];
+    }
+    stream[9] = (data.meta.id >> 8) & 0xFF;
+    stream[10] = data.meta.id & 0xFF;
+    stream[11] = data.meta.index_packet;
+    stream[12] = data.meta.total_packet_s;
+    stream[13] = data.length;
+    for (int i = 0; i < data.length; i++){
+        stream[14 + i] = data.data[i];
+    }
+    return stream;
+}
+
+ACK_T streamToAck(u8_t* stream){
+    ACK_T ack;
+    ack.meta.crc16 = (stream[1] << 8) | stream[2];
+    for (int i = 0; i < 6; i++){
+        ack.meta.manager_mac[i] = stream[3 + i];
+    }
+    ack.meta.id = (stream[9] << 8) | stream[10];
+    ack.meta.index_packet = stream[11];
+    ack.meta.total_packet_s = stream[12];
+    return ack;
+}
+
+u16_t crc16(u8_t *data_p, u8_t length)
+{
+    u8_t x;
+    u16_t crc = 0xFFFF;
+
+    while (length--){
+        x = crc >> 8 ^ *data_p++;
+        x ^= x >> 4;
+        crc = (crc << 8) ^ ((u16_t)(x << 12)) ^ ((u16_t)(x << 5)) ^ ((u16_t)x);
+    }
+
+    return crc;
+}
--- lib/DataPacker/DataPacker.h
+++ lib/DataPacker/DataPacker.h
@@ -1,21 +1,71 @@
 #ifndef DATAPACKER_H
 #define DATAPACKER_H
 
+#include <Arduino.h>
+
 /*
-* Pack the data into a byte array
-*
-* Structure:
-*
-* Start byte                    >> 0x7E                         >> 1 byte
-* Mode                          >> 0x00 for SYN, 0x01 for ACK   >> 1 byte
-* Data length                   >> n                            >> 1 byte
-* Data                          >> data w src MAC (encripted)   >> n bytes (between 100 and 255 bytes we'll see)
-* CRC                           >> crc16                        >> 2 bytes
-* Destination                   >> MAC address                  >> 6 bytes
-* Packet ID                     >> internal counter             >> 2 bytes
-* Packet index                  >> packet counter               >> 1 byte
-* Total packets                 >> total packets                >> 1 byte
-* End byte                      >> 0x7F                         >> 1 byte
+* @brief Structure for the meta data of a packet
+* @param crc16: CRC16 checksum of the data
+* @param manager_mac: MAC address of the manager
+* @param id: ID of the packet
+* @param index_packet: Index of the packet
+* @param total_packet_s: Total packets
 */
+struct META_T{
+    u16_t crc16;
+    u8_t manager_mac[6];
+    u16_t id;
+    u8_t index_packet;
+    u8_t total_packet_s;
+};
+const int META_SIZE = sizeof(META_T);
+
+/*
+* @brief Structure for a data packet
+* @param type: Type of the packet
+* @param meta: Meta data of the packet
+* @param length: Length of the data
+* @param data: Data of the packet
+*/
+struct DAT_T{
+    u8_t type = 0x0D;
+    META_T meta;
+    u8_t length;
+    u8_t* data;
+};
+const int DAT_SIZE = sizeof(DAT_T);
+
+/*
+* @brief Structure for an ACK packet
+* @param type: Type of the packet
+* @param meta: Meta data of the packet
+*/
+struct ACK_T{
+    u8_t type = 0x0A;
+    META_T meta;
+};
+const int ACK_SIZE = sizeof(ACK_T);
+
+/*
+* @brief Create a stream from a DAT_T struct
+* @param data: DAT_T struct
+* @return u8_t* stream
+*/
+u8_t* dataToStream(DAT_T data);
+
+/*
+* @brief Create a DAT_T struct from a stream
+* @param stream: u8_t* stream
+* @return DAT_T struct
+*/
+ACK_T streamToAck(u8_t* stream);
+
+/*
+* @brief Calculate the CRC16 checksum of a data
+* @param data_p: u8_t* data
+* @param length: u16_t length
+* @return u16_t crc16
+*/
+u16_t crc16(u8_t* data_p, u8_t length);
 
 #endif
