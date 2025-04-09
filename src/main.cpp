5f5ac12fc6a361e6f9fcf2f979f9a7c47a60c076
muffindud <ccatlabuga2002@gmail.com>
2025-04-09 11:11:46 +0300

    refactor main logic

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 #include <ScioSense_ENS16x.h>
 #include <Wire.h>
 
-#include "HC12.h"
-#include "DataPacker.h"
-#include "ManagerPair.h"
+#include "HC12Lib.h"
+#include "DataPackerLib.h"
+#include "ManagerPairLib.h"
 
 #include <config.h>
 
@@ -15,71 +15,66 @@ Adafruit_AHTX0 aht;
 ENS160 ens160;
 
 HC12 hc12(HC_12_RX_PIN, HC_12_TX_PIN, HC_12_SET_PIN);
-MAC_ADDRESS_T worker_mac = {
-    .mac = {0x55, 0x0, 0x0, 0x0, 0x0, 0x1}
-};
-MAC_ADDRESS_T manager_mac;
+
+uint8_t* managerMac;
+uint8_t workerMac[6];
 
 int delaySeconds = 3;
-int p = 0;
 
-void setup(){
-    Wire.begin();
+void beginSensors(){
     bmp.begin();
     aht.begin();
     ens160.begin(&Wire, ENS160_ADDR);
-
-    pinMode(PAIR_BUTTON_PIN, INPUT_PULLUP);
-    manager_mac = getManagerMac();
-
     ens160.startStandardMeasure();
+}
+
+void setup(){
+    Wire.begin();
+    beginSensors();
+
+    managerMac = getManagerMac();
+    esp_efuse_mac_get_default(workerMac);
 
     delay(500);
 }
 
 void loop(){
-    // if(digitalRead(PAIR_BUTTON_PIN) == LOW){
-    //     pairManager(worker_mac);
-    //     manager_mac = getManagerMac();
-    // }
+    if(isPairingMode()){
+        exchangeManagerCreds();
+        managerMac = getManagerMac();
+        beginSensors();
+    }else{
+        ens160.wait();
+        sensors_event_t tempEvent, humidityEvent;
+        aht.getEvent(&humidityEvent, &tempEvent);
+        ens160.writeCompensation(tempEvent.temperature, humidityEvent.relative_humidity);
+        ens160.update();
 
-    ens160.wait();
-    sensors_event_t temp_event, humidity_event;
-    aht.getEvent(&humidity_event, &temp_event);
-    ens160.writeCompensation(temp_event.temperature, humidity_event.relative_humidity);
-    ens160.update();
+        PACKET_T packet;
 
-    float temperature_c = temp_event.temperature;
-    float humidity_perc = humidity_event.relative_humidity;
-    float pressure_mmHg = bmp.readPressure() / MMHG_TO_PA;
-    float correctedPPM = ens160.getEco2();
+        packet.type = DAT_MODE;
 
+        packet.data.temperature = (uint64_t)((tempEvent.temperature + 40.) * 100.);
+        packet.data.humidity    = (uint64_t)(humidityEvent.relative_humidity * 100.);
+        packet.data.pressure    = (uint64_t)((bmp.readPressure() / MMHG_TO_PA) * 100.);
+        packet.data.ppm         = (uint64_t)(ens160.getEco2() * 100.);
 
-    DAT_T data_packet;
-    RAW_DATA_T data;
-    data.temperature = (uint64_t)((temperature_c + 40.) * 100.);
-    data.humidity = (uint64_t)(humidity_perc * 100.);
-    data.pressure = (uint64_t)(pressure_mmHg * 100.);
-    data.ppm = (uint64_t)(correctedPPM * 100.);
+        packet.meta.crc16 = getCRC16(packet.data);
+        memcpy(packet.meta.worker_mac, &workerMac, 6);
+        memcpy(packet.meta.manager_mac, &managerMac, 6);
+        packet.meta.packet_id = getId(
+            packet.meta.manager_mac,
+            packet.meta.worker_mac,
+            packet.data,
+            packet.meta.crc16
+        );
+        packet.meta.index_packet = 0;
+        packet.meta.total_packet = 1;
 
-    memcpy(data_packet.data, &data, RAW_DATA_SIZE);
+        packet.length = sizeof(DATA_T);
 
-    data_packet.type = DAT_MODE;
-    data_packet.meta.crc16 = crc16(data);
-    for(int i = 0; i < 6; i++){
-        data_packet.meta.manager_mac[i] = manager_mac.mac[i];
+        hc12.sendData(packet);
+
+        delay(delaySeconds * 1000);
     }
-    for(int i = 0; i < 6; i++){
-        data_packet.meta.worker_mac[i] = worker_mac.mac[i];
-    }
-    data_packet.meta.id = getId(data_packet.meta.manager_mac, data_packet.meta.worker_mac, data, data_packet.meta.crc16);
-    data_packet.meta.index_packet = 0;
-    data_packet.meta.total_packet_s = 1;
-
-    data_packet.length = sizeof(DAT_T) - META_SIZE;
-    uint8_t* stream = (uint8_t*)malloc(DAT_SIZE);
-    stream = (uint8_t*)&data_packet;
-    hc12.sendData(stream);
-
-    delay(delaySeconds * 1000);
 }
